17e5749b186f76f73e4b99d795a0ef4a134671ec
Ruhifarhath <112181464+Ruhifarhath@users.noreply.github.com>
2024-07-15 13:35:44 +0530

    Create removeDuplicates.cpp

---
+++ removeDuplicates.cpp
@@ -0,0 +1,27 @@
+##Remove Duplicates from Sorted Array
+
+Given an integer array `nums` sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in `nums`.
+
+Consider the number of unique elements of `nums` to be `k`. To get accepted, you need to do the following things:
+
+1. Change the array `nums` such that the first `k` elements of `nums` contain the unique elements in the order they were present in `nums` initially. The remaining elements of `nums` are not important as well as the size of `nums`.
+2. Return `k`.
+
+###Solution
+
+```cpp
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+
+        int j = 1;  // Pointer to place the next unique element
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
